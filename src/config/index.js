15e2532105bc405be543682759fdf4ced1c8f648
Yanqi-Lin <664506986@qq.com>
2023-12-28 13:27:47 +0800

    reverse proxy

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,5 @@
-const MODE = import.meta.env.MODE // 环境变量
+const MODE = import.meta.env.MODE; // 环境变量
 
-export const baseUrl = MODE == 'development' ? '/api' : 'http://api.chennick.wang'
+// export const baseUrl = MODE == 'development' ? '/api' : 'http://api.chennick.wang'
+export const baseUrl =
+  MODE == "development" ? "/api" : "https://tally-demo.vercel.app";
